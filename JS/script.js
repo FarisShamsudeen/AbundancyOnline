8a19f8c27e78b69e05c93ee0a5bb8025d13044be
FarisShamsudeen <132601449+FarisShamsudeen@users.noreply.github.com>
2023-06-09 20:15:52 +0530

    files

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+let menuBar = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menuBar.onclick = () =>{
+    menuBar.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+document.getElementById("home").scrollIntoView();
+document.getElementById("about").scrollIntoView();
+document.getElementById("teachers").scrollIntoView();
+document.getElementById("reviews").scrollIntoView();
